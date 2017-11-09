ea83c37966031a44739ba616b9d105a2ca15e952
李航 <1244225737@qq.com>
2017-11-09 19:00:57 +0800

    1

---
+++ mod/auth/config.py
@@ -0,0 +1,7 @@
+# coding:utf-8
+# Created by lihang on 2017/3/23.
+
+CHECK_URL1 = "http://jwxt.neuq.edu.cn/xtgl/dl_loginForward.html"
+CHECK_URL2 = "http://jwxt.neuq.edu.cn/xtgl/login_login.htm1l"
+
+TIME_OUT = 8
